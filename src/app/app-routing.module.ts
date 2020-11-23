f866e1bd83b56975c0608dc43021f1adada3ef6f
Jiří Liška <jiri.liska@emeldi.com>
2020-11-23 02:45:47 +0100

    Add FA, bootstrap, material, change css to scss, simple navigation + routerlinks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './web/home/home.component';
+import { DashboardComponent } from './web/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'dashboard', component: DashboardComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './web/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
+import { SideNavigationComponent } from './components/side-navigation/side-navigation.component';
+import { DashboardComponent } from './web/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavigationBarComponent,
+    SideNavigationComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    NgbModule,
+    CommonModule,
+    MatSidenavModule
+  ],
+  exports: [
+    MatSidenavModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
